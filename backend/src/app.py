3c69c9c803c3e8ff626691be8f2d29e4ed53f941
Jordan Witte <wittejm@gmail.com>
2023-05-19 14:40:06 -0700

    puzzle function returns the puzzle_number query param

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 from puzzle.lambda_function import lambda_handler as puzzleHandler
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/puzzle')
+@app.route('/puzzle', methods=['GET'])
 def puzzle():
-    return puzzleHandler({}, {})
+    args = request.args
+    return puzzleHandler({"queryStringParameters":{"puzzle_number":args.get("puzzle_number", default=0, type=int)}}, {})
 
 if __name__ == '__main__':
     app.run()
--- backend/src/puzzle/lambda_function.py
+++ backend/src/puzzle/lambda_function.py
@@ -1,7 +1,10 @@
 import json
 
+
 def lambda_handler(event, context):
+    puzzle_number = event['queryStringParameters']['puzzle_number']
+
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': json.dumps({"puzzleNumber": puzzle_number, "words": ["tears", "petri", "metro"]})
     }
