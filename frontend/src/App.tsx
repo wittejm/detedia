d5e570a9cb8aacdd0a6e452599808e8d5306e0d3
Jordan Witte <wittejm@gmail.com>
2023-05-20 14:30:36 -0700

    redirect to closest puzzle number

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { useRoutes } from "react-router-dom";
+import { Outlet, useRoutes } from "react-router-dom";
+import PuzzleLoader from "./PuzzleLoader";
 import PuzzlePage from "./PuzzlePage";
-
 
 export default function App() {
   const routedPages = useRoutes([
     {
       path: "/",
-      element: <PuzzlePage />,
-    },
-    {
-      path: "/puzzle/",
-      element: <div>puzzle</div>,
+      element: <Outlet />,
+      children: [
+        {
+          index: true,
+          element: <PuzzlePage />,
+        },
+        {
+          path: ":puzzleId",
+          element: <PuzzleLoader/>,
+        },
+      ],
     },
   ]);
   return routedPages;
