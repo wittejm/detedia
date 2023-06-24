dcb7329f8ce5e436d570b922e29a8387fc0fcc7a
Jordan Witte <wittejm@gmail.com>
2023-06-23 21:44:21 -0400

    tweaked maker WIP

--- frontend/src/Maker.tsx
+++ frontend/src/Maker.tsx
@@ -1,22 +1,93 @@
-import { TextField } from "@mui/material";
-import React from "react";
+import { QuestionMark } from "@mui/icons-material";
+import { Button, Grid, TextField, Tooltip, Typography } from "@mui/material";
+import Item from "@mui/material/Grid";
+import React, { useState } from "react";
 
 export default function Maker() {
+
+  const [tagValue, setTagValue] = useState("");
+  const [nameValue, setNameValue] = useState("");
+  const [wordsValue, setWordsValue] = useState("");
+  
   return (
     <div>
-      <div>Puzzle Maker</div>
-      <div>
-        Id:
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </div>
-      <div>
-        Name:
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </div>
-      <div>
-        Words:
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </div>
+      <h3>Puzzle Maker</h3>
+      <Grid
+        container
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        justifyContent="flex-end"
+      >
+        <Grid item xs={6} display="flex" justifyContent="flex-end">
+          <Item>
+            Tag:
+            <Tooltip
+              title={
+                <Typography fontSize={16}>
+                  {" "}
+                  This is used for the puzzle's URL. Alphanumeric or hyphen
+                  characters only.
+                </Typography>
+              }
+            >
+              <QuestionMark fontSize={"inherit"} />
+            </Tooltip>
+          </Item>
+        </Grid>
+        <Grid item xs={6} display="flex" justifyContent="flex-start">
+          <Item>
+            <TextField id="tagField" label="" variant="outlined" value={tagValue} onChange={(e)=>setTagValue(e.target.value)} />
+          </Item>
+        </Grid>
+        <Grid item xs={6} display="flex" justifyContent="flex-end">
+          <Item>
+            Name:
+            <Tooltip
+              title={
+                <Typography fontSize={16}>
+                  {" "}
+                  This can be anything. It appears as the page header.
+                </Typography>
+              }
+            >
+              <QuestionMark fontSize={"inherit"} />
+            </Tooltip>
+          </Item>
+        </Grid>
+        <Grid item xs={6} display="flex" justifyContent="flex-start">
+          <Item>
+            <TextField id="nameField" label="" variant="outlined" value={nameValue} onChange={(e)=>setNameValue(e.target.value)} />
+          </Item>
+        </Grid>
+        <Grid item xs={6} display="flex" justifyContent="flex-end">
+          <Item>
+            Words:
+            <Tooltip
+              title={
+                <Typography fontSize={16}>
+                  {" "}
+                  Space- or comma-separated, 1-7 words, all of length 5, no
+                  punctuation.{" "}
+                </Typography>
+              }
+            >
+              <QuestionMark fontSize={"inherit"} />
+            </Tooltip>
+          </Item>
+        </Grid>
+        <Grid item xs={6} display="flex" justifyContent="flex-start">
+          <Item>
+            <TextField id="wordsField" label="" variant="outlined"  value={wordsValue} onChange={(e)=>setWordsValue(e.target.value)}/>
+          </Item>
+        </Grid>
+        <Grid item xs={6} display="flex" justifyContent="flex-end"></Grid>
+        <Grid item xs={6} display="flex" justifyContent="flex-start">
+          <Item>
+            <Button size="large" variant="contained" onClick={()=>0}>Create!</Button>
+          </Item>
+        </Grid>
+      </Grid>
     </div>
   );
 }
