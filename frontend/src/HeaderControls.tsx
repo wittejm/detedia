90fcee5856843810be20c1ee6e454123d7c7ce16
Jordan Witte <wittejm@gmail.com>
2024-02-08 12:54:34 -0800

    num of ! equal to violations

--- frontend/src/HeaderControls.tsx
+++ frontend/src/HeaderControls.tsx
@@ -53,7 +53,7 @@ function HeaderControls({ activePuzzleIndex }: Props) {
         />
         <div className="headerText">
           {data[activePuzzleIndex].puzzleNumber}
-          {obeysSuperHardMode(activePuzzleIndex) ? "" : "!"}
+          {"!".repeat(numSuperHardModeViolations(activePuzzleIndex)) }
         </div>
         <NavButton
           text=">"
@@ -85,11 +85,43 @@ function NavButton({ text, onClick }: NavButtonProps) {
 }
 export default HeaderControls;
 
-function obeysSuperHardMode(activePuzzleIndex: number) {
+function numSuperHardModeViolations(activePuzzleIndex: number) {
   // for each word: for each blank, check that subsequent words do not contain that letter. UNLESS there's yellow shit going on.
-
+  let numViolations = 0;
   const sourceWords = data[activePuzzleIndex].words;
   const trueWord = sourceWords[sourceWords.length - 1];
+  sourceWords.forEach((previousWord, previousIndex)=>
+  {
+    sourceWords.slice(previousIndex+1).forEach((subsequentWord)=>{
+      const byg = computeByg(previousWord, trueWord);
+      byg.forEach((c, ind) =>
+      {
+        const indicesInPreviousWordOfThisLetter = previousWord
+          .split("")
+          .reduce((soFar, current, currInd) => {
+            if (current === previousWord[ind]) soFar.push(currInd);
+            return soFar;
+          }, [] as number[]);
+        const numYellowOrGreenOfThisLetterInPreviousWord =
+          indicesInPreviousWordOfThisLetter.filter(
+            (ind) => byg[ind] === "yellow" || byg[ind] === "green",
+          ).length;
+        const foundARepeat =
+          (c === "yellow" && previousWord[ind] === subsequentWord[ind]) ||
+          (c === "gray" &&
+            (numYellowOrGreenOfThisLetterInPreviousWord <
+              (subsequentWord.match(new RegExp(previousWord[ind], "g")) || [])
+                .length ||
+              previousWord[ind] === subsequentWord[ind]));
+        if (foundARepeat) {
+          numViolations++;
+        };
+      })
+    })
+  }
+  )
+  if (Math.random()) return numViolations;
+
   let foundRepeat = sourceWords.some((previousWord, previousIndex) =>
     sourceWords.slice(previousIndex + 1).some((subsequentWord) => {
       const byg = computeByg(previousWord, trueWord);
@@ -116,5 +148,6 @@ function obeysSuperHardMode(activePuzzleIndex: number) {
       });
     }),
   );
-  return !foundRepeat;
+  return 0;
+  // return !foundRepeat;
 }
