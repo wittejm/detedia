288e9fddae3b9581d0aab397545858ff00936021
Jordan Witte <wittejm@gmail.com>
2025-06-22 06:33:07 -0700

    fix for duckduckgo

--- frontend/src/PuzzlePage.tsx
+++ frontend/src/PuzzlePage.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
-import HeaderControls, {numSuperHardModeViolations, numSuperHardModeViolationsForPuzzleIndex} from "./HeaderControls";
+import HeaderControls, {
+  numSuperHardModeViolations,
+  numSuperHardModeViolationsForPuzzleIndex,
+} from "./HeaderControls";
 import Keyboard from "./Keyboard";
 import Puzzle from "./Puzzle";
 import Submissions from "./Submissions";
 import "./App.css";
 // @ts-ignore
-import domtoimage from 'dom-to-image-more';
-import { useRef } from 'react';
+import domtoimage from "dom-to-image-more";
+import { useRef } from "react";
 
 function submitGuess(
   guess: string[],
@@ -15,31 +18,47 @@ function submitGuess(
   setSubmissions: any,
   activePuzzleIndex: number,
   source: string,
-  data: { puzzleNumber: number, words: string[] }[]
+  data: { puzzleNumber: number; words: string[] }[],
 ) {
-  const puzzleIsSuperHardMode = numSuperHardModeViolationsForPuzzleIndex(activePuzzleIndex, data) == 0;
-  const joinedGuess: string[] = []
-  for(let i=0; i<guess.length/5; i++) {
-    joinedGuess.push(guess.slice(5*i, 5*(i+1)).join('').toLowerCase())
+  const puzzleIsSuperHardMode =
+    numSuperHardModeViolationsForPuzzleIndex(activePuzzleIndex, data) == 0;
+  const joinedGuess: string[] = [];
+  for (let i = 0; i < guess.length / 5; i++) {
+    joinedGuess.push(
+      guess
+        .slice(5 * i, 5 * (i + 1))
+        .join("")
+        .toLowerCase(),
+    );
   }
-  const submissionIsSuperHardMode = numSuperHardModeViolations(joinedGuess, joinedGuess[joinedGuess.length-1]) == 0;
+  const submissionIsSuperHardMode =
+    numSuperHardModeViolations(
+      joinedGuess,
+      joinedGuess[joinedGuess.length - 1],
+    ) == 0;
   if (puzzleIsSuperHardMode && !submissionIsSuperHardMode) {
-    const proceed = confirm('This answer does not qualify as super hard mode. Submit anyway?')
+    const proceed = confirm(
+      "This answer does not qualify as super hard mode. Submit anyway?",
+    );
     if (!proceed) {
-      return
+      return;
     }
   }
   const newSubmissions = [guess, ...submissions];
   saveGuesses(activePuzzleIndex, newSubmissions, source, data);
   setSubmissions(newSubmissions);
-  const correctLetters = getCorrectLettersFromGuess(guess, activePuzzleIndex, data);
+  const correctLetters = getCorrectLettersFromGuess(
+    guess,
+    activePuzzleIndex,
+    data,
+  );
   setGuess(correctLetters);
 }
 
 type Props = {
   activePuzzleIndex: number;
   source: string;
-  data: { puzzleNumber: number, words: string[] }[]
+  data: { puzzleNumber: number; words: string[] }[];
 };
 
 function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
@@ -57,7 +76,7 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
       const correctLetters = getCorrectLettersFromGuess(
         priorSubmissions[0],
         puzzleIndex,
-        data
+        data,
       );
       setGuess(correctLetters);
     } else {
@@ -68,7 +87,7 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
 
   async function captureAndShare() {
     if (!ref.current) {
-      console.log("no ref")
+      console.log("no ref");
       return;
     }
 
@@ -76,22 +95,23 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
       const blob = await domtoimage.toBlob(ref.current, {
         style: {
           // optional tweaks if needed
-          transform: 'scale(1)',
-          transformOrigin: 'top left',
+          transform: "scale(1)",
+          transformOrigin: "top left",
         },
         height: ref.current.scrollHeight, // important for tall content
         width: ref.current.scrollWidth,
       });
 
-      const file = new File([blob], 'screenshot.png', { type: 'image/png' });
-
+      const file = new File([blob], "screenshot.png", { type: "image/png" });
+      const blobUrl = URL.createObjectURL(blob);
       if (navigator.canShare?.({ files: [file] })) {
         await navigator.share({
           files: [file],
-          title: 'My letter',
-          text: 'Check this out',
+          title: "My letter",
+          text: "Check this out",
         });
       } else {
+        /*
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = url;
@@ -100,9 +120,23 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
         a.click();
         document.body.removeChild(a);
         URL.revokeObjectURL(url);
+        */
+        const a = document.createElement("a");
+        if ("download" in a) {
+          a.href = blobUrl;
+          a.download = "screenshot.png";
+          // some mobile browsers require the link to be in-DOM
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+          URL.revokeObjectURL(blobUrl);
+          return;
+        }
+        window.open(blobUrl, '_blank', 'noopener,noreferrer');
+    
       }
     } catch (err) {
-      console.error('Failed to capture/share', err);
+      console.error("Failed to capture/share", err);
     }
   }
 
@@ -110,7 +144,12 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
 
   return (
     <div className="DetediaPage" ref={ref}>
-      <HeaderControls activePuzzleIndex={activePuzzleIndex} source={source} data={data} captureAndShare={captureAndShare} />
+      <HeaderControls
+        activePuzzleIndex={activePuzzleIndex}
+        source={source}
+        data={data}
+        captureAndShare={captureAndShare}
+      />
       <Puzzle
         activePuzzleIndex={activePuzzleIndex}
         guess={guess}
@@ -124,8 +163,8 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
             submissions,
             setSubmissions,
             activePuzzleIndex,
-            source, 
-            data
+            source,
+            data,
           )
         }
         data={data}
@@ -141,7 +180,7 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
           activePuzzleIndex,
           submissions,
           source,
-          data
+          data,
         )}
         submissions={submissions}
         data={data}
@@ -155,7 +194,7 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
             setSubmissions,
             activePuzzleIndex,
             source,
-            data
+            data,
           )
         }
       >
@@ -168,7 +207,7 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
       />
       <button
         onClick={() => {
-          setGuess(Array(5 * data[activePuzzleIndex].words.length).fill(" "))
+          setGuess(Array(5 * data[activePuzzleIndex].words.length).fill(" "));
           clearGuesses(activePuzzleIndex, source, data);
           setSubmissions([]);
         }}
@@ -181,9 +220,15 @@ function PuzzlePage({ activePuzzleIndex, source, data }: Props) {
 
 export default PuzzlePage;
 
-export function getProgressFromStorage(activePuzzleIndex: number, source: string, data: { puzzleNumber: number, words: string[] }[]) {
+export function getProgressFromStorage(
+  activePuzzleIndex: number,
+  source: string,
+  data: { puzzleNumber: number; words: string[] }[],
+) {
   const allSubmissionsString = window.localStorage.getItem(
-    `submittedGuesses-${source==="poems" ? "poems-" : ""}${data[activePuzzleIndex].puzzleNumber}`,
+    `submittedGuesses-${source === "poems" ? "poems-" : ""}${
+      data[activePuzzleIndex].puzzleNumber
+    }`,
   );
   const res =
     allSubmissionsString &&
@@ -191,18 +236,31 @@ export function getProgressFromStorage(activePuzzleIndex: number, source: string
   return res || [];
 }
 
-function saveGuesses(activePuzzleIndex: number, allSubmissions: string[][], source: string, data: { puzzleNumber: number, words: string[] }[]) {
+function saveGuesses(
+  activePuzzleIndex: number,
+  allSubmissions: string[][],
+  source: string,
+  data: { puzzleNumber: number; words: string[] }[],
+) {
   const allSubmissionsString = allSubmissions
     .map((guessSet) => guessSet.join(""))
     .join(":");
   window.localStorage.setItem(
-    `submittedGuesses-${source==="poems" ? "poems-" : ""}${data[activePuzzleIndex].puzzleNumber}`,
+    `submittedGuesses-${source === "poems" ? "poems-" : ""}${
+      data[activePuzzleIndex].puzzleNumber
+    }`,
     allSubmissionsString,
   );
 }
-function clearGuesses(activePuzzleIndex: number, source: string, data: { puzzleNumber: number, words: string[] }[]) {
+function clearGuesses(
+  activePuzzleIndex: number,
+  source: string,
+  data: { puzzleNumber: number; words: string[] }[],
+) {
   window.localStorage.setItem(
-    `submittedGuesses-${source==="poems" ? "poems-" : ""}${data[activePuzzleIndex].puzzleNumber}`,
+    `submittedGuesses-${source === "poems" ? "poems-" : ""}${
+      data[activePuzzleIndex].puzzleNumber
+    }`,
     "",
   );
   getProgressFromStorage(activePuzzleIndex, source, data);
@@ -212,7 +270,7 @@ function getUsageFromSubmissions(
   activePuzzleIndex: number,
   submissions: string[][],
   source: string,
-  data: { puzzleNumber: number, words: string[] }[]
+  data: { puzzleNumber: number; words: string[] }[],
 ) {
   const letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
   const res = letters
@@ -227,7 +285,7 @@ function getUsageFromSubmissions(
               activePuzzleIndex,
               submissions,
               wordIndex,
-              data
+              data,
             ),
           )),
         acc
@@ -242,7 +300,7 @@ function getKeyColorFromSubmissions(
   activePuzzleIndex: number,
   submissions: string[][],
   wordIndex: number,
-  data: { puzzleNumber: number, words: string[] }[]
+  data: { puzzleNumber: number; words: string[] }[],
 ) {
   let color = "white";
   const sourceWord = data[activePuzzleIndex].words[wordIndex].toUpperCase();
@@ -267,7 +325,7 @@ function getKeyColorFromSubmissions(
 function getCorrectLettersFromGuess(
   guess: string[],
   activePuzzleIndex: number,
-  data:  { puzzleNumber: number, words: string[] }[]
+  data: { puzzleNumber: number; words: string[] }[],
 ) {
   return guess.map((letter, index) => {
     const wordIndex = Math.floor(index / 5);
